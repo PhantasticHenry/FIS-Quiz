be2874626ebcd75e34cf8a8f01513898864633af
github username <github email address>
2020-12-08 16:49:01 -0800

    Route PageNotFound added

--- frontend-quiz/src/App.js
+++ frontend-quiz/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import Home from "./features/home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/navbar";
+import PageNotFound from "./features/pageNotFound";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Switch>
-          <Navbar />
           <Route exact path="/" component={Home} />
+          <Route component={PageNotFound} />
         </Switch>
       </Router>
     </div>
