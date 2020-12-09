27f4e93951ce2f10fda0a45a2c7b4f73cb52210c
github username <github email address>
2020-12-08 17:55:07 -0800

    created questions controller with CRUD

--- backend-quiz-api/app/controllers/questions_controller.rb
+++ backend-quiz-api/app/controllers/questions_controller.rb
@@ -1,2 +1,45 @@
 class QuestionsController < ApplicationController
+  before_action :set_question
+
+  def index 
+    @questions = Question.all 
+    render json: @questions
+  end
+
+  def create 
+    question = Question.new(question_params)
+    if question.save! 
+      render json: question 
+    else 
+      render json: {error: "An error occured during creation..."}
+    end
+  end
+
+  def show 
+    if @question
+      render json: @question 
+    else 
+      render json: {error: "Unable to find with ID"}
+    end
+  end
+
+  def update 
+    if @question.update
+      render json: @question
+    else 
+      render json: {error: "Unable to find with ID"}
+    end
+  end
+
+  def delete 
+    @question.destroy
+  end
+
+  private
+    def set_question
+      @question = Question.find_by(id: params[:id])
+    end
+    def question_params
+      params.require(:question).permit(:category, :question, :correct_answer, :incorrect_answers)
+    end
 end
