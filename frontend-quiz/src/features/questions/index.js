04d88bfdeb5cd6edb60c27f21c0a6650c5737f4e
github username <github email address>
2020-12-08 21:34:00 -0800

    added actionTypes for questions

---
+++ frontend-quiz/src/features/questions/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+function Questions() {
+  const dispatch = useDispatch();
+
+  return <div>All questions here.....</div>;
+}
+
+export default Questions;
---
+++ frontend-quiz/src/features/startQuiz/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./StartQuiz.module.css";
+
+function StartQuiz() {
+  return <div>Starting Quiz......</div>;
+}
+
+export default StartQuiz;
