ed17234f5c06a0c0b79ab823ce99d3fc51d2de60
github username <github email address>
2020-12-09 14:33:47 -0800

    add Question component and styles

--- frontend-quiz/src/features/questions/Question.js
+++ frontend-quiz/src/features/questions/Question.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Questions.module.css";
 
-function Question() {
-  return <div>Questions component</div>;
+function Question({ question }) {
+  const [flip, setFlip] = useState(false);
+
+  const options = [...question.incorrect_answers, question.correct_answer];
+
+  const questionFront = (
+    <div className={styles.front}>
+      {question.question}
+      <div className={styles.questionOptions}>
+        {options.map((option) => {
+          return (
+            <div className={styles.questionOption} key={option + Date.now()}>
+              {option}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+
+  return (
+    <div
+      className={`${styles.card} ${flip ? styles.flip : ""}`}
+      onClick={() => setFlip(!flip)}
+    >
+      <div className={styles.back}>{question.answer}</div>
+      {flip ? question.correct_answer : questionFront}
+    </div>
+  );
 }
 
 export default Question;
