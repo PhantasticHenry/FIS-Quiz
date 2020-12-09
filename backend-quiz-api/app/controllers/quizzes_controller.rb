0321bc0bf13bb70b7b71906fa5b52e375bb19ffc
github username <github email address>
2020-12-08 21:00:13 -0800

    added actions to quizzes controller

--- backend-quiz-api/app/controllers/quizzes_controller.rb
+++ backend-quiz-api/app/controllers/quizzes_controller.rb
@@ -1,2 +1,25 @@
 class QuizzesController < ApplicationController
+  def index 
+    @quizzes = Quiz.all 
+    render json: @quizzes
+  end
+
+  def create
+    questions = Question.all
+    quiz = Quiz.new(quiz_params)
+    questions.each do |question|
+      qq = quiz.quiz_questions.build(question_id: question.id)
+      qq.save!
+    end
+    if quiz.save!
+      render json: quiz
+    else 
+      render json: {error: "An error occured during creation..."}
+    end
+  end
+
+  private 
+  def quiz_params
+    params.require(:quiz).permit(:player, :score)
+  end
 end
