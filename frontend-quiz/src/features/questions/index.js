6108fcfe3423bb94972c56a7ff7c9ce2b4ed929f
github username <github email address>
2020-12-08 22:36:43 -0800

    useEffect to dispatch get questions details

--- frontend-quiz/src/features/questions/index.js
+++ frontend-quiz/src/features/questions/index.js
@@ -1,10 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { getQuestions } from "../../actions/getQuestions";
+import Question from "./Question";
 
 function Questions() {
   const dispatch = useDispatch();
+  const questions = useSelector((state) => state.questions);
 
-  return <div>All questions here.....</div>;
+  const question = questions.map((question) => (
+    <Question question={question} key={question.id} />
+  ));
+
+  useEffect(() => {
+    dispatch(getQuestions());
+  }, []);
+
+  return <div>{question}</div>;
 }
 
 export default Questions;
