653298323db58b082f322debc773bb3a10621d1f
github username <github email address>
2020-12-08 16:59:00 -0800

    NavLinks added for future components

--- frontend-quiz/src/components/navbar/index.js
+++ frontend-quiz/src/components/navbar/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+import logo from "../../images/logo.png";
 
 function Navbar() {
-  return <div className={styles.container}>This is my navbar!!!!</div>;
+  return (
+    <nav className={styles.navContainer}>
+      <div className={styles.logo}>
+        <NavLink to="/">
+          {/* fix why logo isn't loading image */}
+          <h1 className={styles.logo}>Logo Here</h1>
+        </NavLink>
+      </div>
+      <ul className={styles.navMenu}>
+        <li className={styles.links}>
+          <NavLink to="/start-game">Start Game</NavLink>
+        </li>
+        <li className={styles.links}>
+          <NavLink to="/high-scores">High Scores</NavLink>
+        </li>
+        <li className={styles.links}>
+          <NavLink to="/questions">All Questions</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
